05ce70142e05aebcf71ce08c21b5f63edfb4697b
Camille Scott <camille.scott.w@gmail.com>
2015-10-14 16:35:20 -0700

    Fix error with dependency loc logging

--- dammit/dependencies.py
+++ dammit/dependencies.py
@@ -21,69 +21,69 @@ def check_system_path():
 
     hmmscan = which('hmmscan')
     hmmpress = which('hmmpress')
-    logger.debug('hmmscan:' + hmmscan)
-    logger.debug('hmmpress:' + hmmpress)
     if hmmscan is None or hmmpress is None:
         deps['HMMER'] = False
     else:
         deps['HMMER'] = True
+        logger.debug('hmmscan:' + hmmscan)
+        logger.debug('hmmpress:' + hmmpress)
 
     cmscan = which('cmscan')
     cmpress = which('cmpress')
-    logger.debug('cmscan:' + cmscan)
-    logger.debug('cmpress:' + cmpress)
     if cmscan is None or cmpress is None:
         deps['Infernal'] = False
     else:
         deps['Infernal'] = True
+        logger.debug('cmscan:' + cmscan)
+        logger.debug('cmpress:' + cmpress)
 
     blastp = which('blastp')
     blastx = which('blastx')
     tblastn = which('tblastn')
     makeblastdb = which('makeblastdb')
-    logger.debug('blastp:' + blastp)
-    logger.debug('blastx:' + blastx)
-    logger.debug('tblastn:' + tblastn)
-    logger.debug('makeblastdb:' + makeblastdb)
     if (blastp is None) or (blastx is None) \
         or (tblastn is None) or (makeblastdb is None):
-
+        
         deps['BLAST+'] = False
     else:
         deps['BLAST+'] = True
+        logger.debug('blastp:' + blastp)
+        logger.debug('blastx:' + blastx)
+        logger.debug('tblastn:' + tblastn)
+        logger.debug('makeblastdb:' + makeblastdb)
 
     busco = which('BUSCO_v1.1b1.py')
-    logger.debug('BUSCO:' + busco)
     if busco is None:
         deps['BUSCO'] = False
     else:
         deps['BUSCO'] = True
+        logger.debug('BUSCO:' + busco)
 
 
     longorfs = which('TransDecoder.LongOrfs')
     predict = which('TransDecoder.Predict')
-    logger.debug('TransDecoder.LongOrfs:' + longorfs)
-    logger.debug('TransDecoder.Predict:' + predict)
     if longorfs is None or predict is None:
         deps['TransDecoder'] = False
     else:
         deps['TransDecoder'] = True
+        logger.debug('TransDecoder.LongOrfs:' + longorfs)
+        logger.debug('TransDecoder.Predict:' + predict)
 
     lastdb = which('lastdb')
     lastal = which('lastal')
-    logger.debug('lastal:' + lastal)
-    logger.debug('latsdb:' + lastdb)
     if lastdb is None or lastal is None:
         deps['LAST'] = False
     else:
         deps['LAST'] = True
+        logger.debug('lastal:' + lastal)
+        logger.debug('latsdb:' + lastdb)
 
     crb_blast = which('crb-blast')
-    logger.debug('crb-blast:' + crb_blast)
     if crb_blast is None:
         deps['crb-blast'] = False
     else:
         deps['crb-blast'] = True
+        logger.debug('crb-blast:' + crb_blast)
 
     return deps
 
