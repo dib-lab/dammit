bf707d8e745546772fcd3439ab5ed7691f5afd03
Camille Scott <camille.scott.w@gmail.com>
2015-10-30 15:52:31 -0700

    Refactor to OO based design

--- dammit/databases.py
+++ dammit/databases.py
@@ -11,154 +11,177 @@ from tasks import get_download_and_gunzip_task, \
                   get_hmmpress_task, \
                   get_cmpress_task, \
                   get_download_and_untar_task, \
-                  get_lastdb_task
+                  get_lastdb_task, \
+                  print_tasks
 
-logger = logging.getLogger(__name__)
 
-def get_dir(args):
-    # By default, we store databases in the home directory
-    db_dir = args.database_dir
-    if db_dir is None:
-        db_dir = os.path.join(common.get_dammit_dir(), 
-                              common.CONFIG['settings']['db_dir'])
-    return os.path.abspath(db_dir)
+class DatabaseHandler(object):
 
-def get_tasks(db_dir, args):
-    '''Generate tasks for installing the bundled databases. 
-    
-    These tasks download the databases, unpack them, and format them for use.
-    Current bundled databases are:
+    def __init__(self, args):
 
-        * Pfam-A (protein domans)
-        * Rfam (RNA models)
-        * OrthoDB8 (conserved ortholog groups)
-        * uniref90 (protiens, if --full selected)
-    
-    User-supplied databases are downloaded separately.
+        self.args = args
+        self.logger = logging.getLogger(self.__class__.__name__)
 
-    Args:
-        db_dir (str): Directory where the databases will be stored.
-        busco_db (str): The BUSCO group to use.
-        full (bool): Whether to do a full run and get UNIREF90 as well.
-
-    Returns:
-        dict: A dictionary of the final database paths.
-        list: A list of the doit tasks.
-
-    '''
-
-    tasks = []
-    databases = {}
-
-    # Get Pfam-A and prepare it for use with hmmer
-    PFAM = os.path.join(db_dir, common.DATABASES['pfam']['filename'])
-    tasks.append(
-        get_download_and_gunzip_task(common.DATABASES['pfam']['url'], PFAM)
-    )
-    tasks.append(
-        get_hmmpress_task(PFAM, common.CONFIG['settings']['hmmer'])
-    )
-    databases['PFAM'] = os.path.abspath(PFAM)
-
-    # Get Rfam and prepare it for use with Infernal
-    RFAM = os.path.join(db_dir, common.DATABASES['rfam']['filename'])
-    tasks.append(
-        get_download_and_gunzip_task(common.DATABASES['rfam']['url'], RFAM)
-    )
-    tasks.append(
-        get_cmpress_task(RFAM, common.CONFIG['settings']['infernal'])
-    )
-    databases['RFAM'] = os.path.abspath(RFAM)
-
-    # Get OrthoDB and prepare it for BLAST use
-    ORTHODB = os.path.join(db_dir, common.DATABASES['orthodb']['filename'])
-    tasks.append(
-        get_download_and_gunzip_task(common.DATABASES['orthodb']['url'], ORTHODB)
-    )
-
-    lastdb_cfg = common.CONFIG['settings']['last']['lastdb']
-    tasks.append(
-        get_lastdb_task(ORTHODB, ORTHODB + '.db', lastdb_cfg, prot=True)
-    )
-    ORTHODB += '.db'
-    databases['ORTHODB'] = os.path.abspath(ORTHODB)
-
-    ORTHODB_GENES = os.path.join(db_dir,
-                                 common.DATABASES['orthodb_genes']['filename'])
-    tasks.append(
-        get_download_and_gunzip_task(common.DATABASES['orthodb_genes']['url'],
-                                     ORTHODB_GENES)
-    )
-    databases['ORTHODB_GENES'] = os.path.abspath(ORTHODB_GENES)
-
-    # A little confusing. First, we get the top-level BUSCO path:
-    BUSCO = os.path.join(db_dir, 'buscodb')
-    tasks.append(
-        # That top-level path is given to the download task:
-        get_download_and_untar_task(common.DATABASES['busco'][args.busco_group]['url'], 
-                                    BUSCO,
-                                    label=args.busco_group)
-    )
-    # The untarred arhive has a folder named after the group:
-    databases['BUSCO'] = os.path.abspath(os.path.join(BUSCO, args.busco_group))
-
-    # Get uniref90 if the user specifies
-    if args.full:
-        UNIREF = os.path.join(common.DATABASES['uniref90']['filename'])
-        tasks.append(
-            get_download_and_gunzip_task(common.DATABASES['uniref90']['url'], UNIREF)
-        )
-        tasks.append(
-            get_blast_format_task(UNIREF, UNIREF + '.db',
-                                  common.DATABASES['uniref90']['db_type'])
-        )
-        UNIREF += '.db'
-        databases['UNIREF'] = os.path.abspath(UNIREF)
-
-    return databases, tasks
-
-def get_doit_config(db_dir):
-
-    doit_config = {
-                    'reporter': common.LogReporter(logger),
-                    'backend': common.DOIT_BACKEND,
-                    'verbosity': common.DOIT_VERBOSITY,
-                    'dep_file': os.path.join(db_dir, 'databases.doit.db')
-                  }
-    logger.debug('doit_config:{0}'.format(doit_config))
-    return doit_config
-
-def run_tasks(db_dir, tasks, args=['run']):
-    
-    doit_config = get_doit_config(db_dir)
-    common.run_tasks(tasks, args, config=doit_config)
-
-def do_check(db_dir, args):
-
-    common.print_header('Checking for database prep (dir: {0})'.format(db_dir),
-                        level=2)
-
-    databases, tasks = get_tasks(db_dir, args)
-
-    doit_config = get_doit_config(db_dir)
-    dep_manager = Dependency(SqliteDB, doit_config['dep_file'])
-    missing = False
-    for task in tasks:
-        status = dep_manager.get_status(task, tasks)
-        logger.debug('{0}:{1}'.format(task.name, status.status))
-        if status.status != 'up-to-date':
-            missing = True
-            logger.warning('[ ] {0}'.format(task.name))
+        if args.database_dir is None:
+            directory = os.path.join(common.get_dammit_dir(), 
+                                     common.CONFIG['settings']['db_dir'])
         else:
-            logger.info('[x] {0}'.format(task.name))
+            directory = args.database_dir
+        self.directory = os.path.abspath(directory)
 
-    common.print_header('Database results', level=2)
+        self.doit_config = {
+                            'reporter': common.LogReporter(self.logger),
+                            'backend': common.DOIT_BACKEND,
+                            'verbosity': common.DOIT_VERBOSITY,
+                            'dep_file': os.path.join(self.directory, 'databases.doit.db')
+                           }
+        self.logger.debug('doit_config:{0}'.format(self.doit_config))
 
-    if missing:
-        logger.warning('Database prep incomplete')
-        common.print_header('to prepare databases, run: dammit databases'\
-                            ' --install', level=2)
-    else:
-        logger.info('All databases prepared!')
+        self.logger.debug('database dir: {0}'.format(self.directory))
+        try:
+            os.mkdir(self.directory)
+        except OSError:
+            self.logger.debug('database dir already exists')
 
-    return databases, tasks, missing
+        self.databases, self.tasks = self.get_tasks()
+
+
+    def handle(self, doit_args=['run']):
+
+        missing = self.check()
+        print_tasks(self.tasks, logger=self.logger)
+        
+        if self.args.install:
+            if missing:
+                common.print_header('Installing databases', level=2)
+                common.run_tasks(self.tasks, doit_args, config=self.doit_config)
+            else:
+                common.print_header('Nothing to install', level=2)
+
+    def check_or_fail(self):
+        missing = self.check()
+        if missing:
+            self.logger.error('INstall databases to continue; exiting')
+            sys.exit(1)
+
+    def check(self):
+
+        common.print_header('Checking for database prep (dir: {0})'.format(self.directory),
+                            level=2)
+
+        dep_manager = Dependency(SqliteDB, self.doit_config['dep_file'])
+        missing = False
+        for task in self.tasks:
+            status = dep_manager.get_status(task, self.tasks)
+            self.logger.debug('{0}:{1}'.format(task.name, status.status))
+            if status.status != 'up-to-date':
+                missing = True
+                self.logger.warning('[ ] {0}'.format(task.name))
+            else:
+                self.logger.info('[x] {0}'.format(task.name))
+
+        common.print_header('Database results', level=2)
+
+        if missing:
+            self.logger.warning('Database prep incomplete')
+            common.print_header('to prepare databases, run: dammit databases'\
+                                ' --install', level=2)
+        else:
+            self.logger.info('All databases prepared!')
+
+        return missing
+
+    def get_tasks(self):
+        '''Generate tasks for installing the bundled databases. 
+        
+        These tasks download the databases, unpack them, and format them for use.
+        Current bundled databases are:
+
+            * Pfam-A (protein domans)
+            * Rfam (RNA models)
+            * OrthoDB8 (conserved ortholog groups)
+            * uniref90 (protiens, if --full selected)
+        
+        User-supplied databases are downloaded separately.
+
+        Args:
+            self.directory (str): Directory where the databases will be stored.
+            busco_db (str): The BUSCO group to use.
+            full (bool): Whether to do a full run and get UNIREF90 as well.
+
+        Returns:
+            dict: A dictionary of the final database paths.
+            list: A list of the doit tasks.
+
+        '''
+
+        tasks = []
+        databases = {}
+
+        # Get Pfam-A and prepare it for use with hmmer
+        PFAM = os.path.join(self.directory, common.DATABASES['pfam']['filename'])
+        tasks.append(
+            get_download_and_gunzip_task(common.DATABASES['pfam']['url'], PFAM)
+        )
+        tasks.append(
+            get_hmmpress_task(PFAM, common.CONFIG['settings']['hmmer'])
+        )
+        databases['PFAM'] = os.path.abspath(PFAM)
+
+        # Get Rfam and prepare it for use with Infernal
+        RFAM = os.path.join(self.directory, common.DATABASES['rfam']['filename'])
+        tasks.append(
+            get_download_and_gunzip_task(common.DATABASES['rfam']['url'], RFAM)
+        )
+        tasks.append(
+            get_cmpress_task(RFAM, common.CONFIG['settings']['infernal'])
+        )
+        databases['RFAM'] = os.path.abspath(RFAM)
+
+        # Get OrthoDB and prepare it for BLAST use
+        ORTHODB = os.path.join(self.directory, common.DATABASES['orthodb']['filename'])
+        tasks.append(
+            get_download_and_gunzip_task(common.DATABASES['orthodb']['url'], ORTHODB)
+        )
+
+        lastdb_cfg = common.CONFIG['settings']['last']['lastdb']
+        tasks.append(
+            get_lastdb_task(ORTHODB, ORTHODB + '.db', lastdb_cfg, prot=True)
+        )
+        ORTHODB += '.db'
+        databases['ORTHODB'] = os.path.abspath(ORTHODB)
+
+        ORTHODB_GENES = os.path.join(self.directory,
+                                     common.DATABASES['orthodb_genes']['filename'])
+        tasks.append(
+            get_download_and_gunzip_task(common.DATABASES['orthodb_genes']['url'],
+                                         ORTHODB_GENES)
+        )
+        databases['ORTHODB_GENES'] = os.path.abspath(ORTHODB_GENES)
+
+        # A little confusing. First, we get the top-level BUSCO path:
+        BUSCO = os.path.join(self.directory, 'buscodb')
+        tasks.append(
+            # That top-level path is given to the download task:
+            get_download_and_untar_task(common.DATABASES['busco'][self.args.busco_group]['url'], 
+                                        BUSCO,
+                                        label=self.args.busco_group)
+        )
+        # The untarred arhive has a folder named after the group:
+        databases['BUSCO'] = os.path.abspath(os.path.join(BUSCO, self.args.busco_group))
+
+        # Get uniref90 if the user specifies
+        if self.args.full:
+            UNIREF = os.path.join(common.DATABASES['uniref90']['filename'])
+            tasks.append(
+                get_download_and_gunzip_task(common.DATABASES['uniref90']['url'], UNIREF)
+            )
+            tasks.append(
+                get_blast_format_task(UNIREF, UNIREF + '.db',
+                                      common.DATABASES['uniref90']['db_type'])
+            )
+            UNIREF += '.db'
+            databases['UNIREF'] = os.path.abspath(UNIREF)
+
+        return databases, tasks
+
--- dammit/dependencies.py
+++ dammit/dependencies.py
@@ -11,102 +11,111 @@ from common import which
 import common
 from tasks import get_download_and_untar_task
 
-logger = logging.getLogger(__name__)
 
+class DependencyHandler(object):
 
-def check_system_path():
-    
-    deps = {}
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
 
-    hmmscan = which('hmmscan')
-    hmmpress = which('hmmpress')
-    if hmmscan is None or hmmpress is None:
-        deps['HMMER'] = False
-    else:
-        deps['HMMER'] = True
-        logger.debug('hmmscan:' + hmmscan)
-        logger.debug('hmmpress:' + hmmpress)
+    def handle(self):
 
-    cmscan = which('cmscan')
-    cmpress = which('cmpress')
-    if cmscan is None or cmpress is None:
-        deps['Infernal'] = False
-    else:
-        deps['Infernal'] = True
-        logger.debug('cmscan:' + cmscan)
-        logger.debug('cmpress:' + cmpress)
+        common.print_header('Checking PATH for dependencies', level=2)
 
-    blastp = which('blastp')
-    blastx = which('blastx')
-    tblastn = which('tblastn')
-    makeblastdb = which('makeblastdb')
-    if (blastp is None) or (blastx is None) \
-        or (tblastn is None) or (makeblastdb is None):
+        system_deps = self.check_system_path()
         
-        deps['BLAST+'] = False
-    else:
-        deps['BLAST+'] = True
-        logger.debug('blastp:' + blastp)
-        logger.debug('blastx:' + blastx)
-        logger.debug('tblastn:' + tblastn)
-        logger.debug('makeblastdb:' + makeblastdb)
+        missing = []
+        for key, status in system_deps.iteritems():
+            if status is False:
+                missing.append(key)
+                self.logger.warning('[ ] {0}'.format(key))
+            else:
+                self.logger.info('[x] {0}'.format(key))
 
-    busco = which('BUSCO_v1.1b1.py')
-    if busco is None:
-        deps['BUSCO'] = False
-    else:
-        deps['BUSCO'] = True
-        logger.debug('BUSCO:' + busco)
+        common.print_header('Dependency results', level=2)
 
-
-    longorfs = which('TransDecoder.LongOrfs')
-    predict = which('TransDecoder.Predict')
-    if longorfs is None or predict is None:
-        deps['TransDecoder'] = False
-    else:
-        deps['TransDecoder'] = True
-        logger.debug('TransDecoder.LongOrfs:' + longorfs)
-        logger.debug('TransDecoder.Predict:' + predict)
-
-    lastdb = which('lastdb')
-    lastal = which('lastal')
-    if lastdb is None or lastal is None:
-        deps['LAST'] = False
-    else:
-        deps['LAST'] = True
-        logger.debug('lastal:' + lastal)
-        logger.debug('latsdb:' + lastdb)
-
-    crb_blast = which('crb-blast')
-    if crb_blast is None:
-        deps['crb-blast'] = False
-    else:
-        deps['crb-blast'] = True
-        logger.debug('crb-blast:' + crb_blast)
-
-    return deps
-
-
-def do_check():
-
-    common.print_header('Checking PATH for dependencies', level=2)
-
-    system_deps = check_system_path()
-    
-    missing = []
-    for key, status in system_deps.iteritems():
-        if status is False:
-            missing.append(key)
-            logger.warning('[ ] {0}'.format(key))
+        if missing:
+            self.logger.warning('{0} missing'.format(', '.join(missing)))
         else:
-            logger.info('[x] {0}'.format(key))
+            self.logger.info('All dependencies satisfied!')
 
-    common.print_header('Dependency results', level=2)
+        return missing
 
-    if missing:
-        logger.warning('{0} missing'.format(', '.join(missing)))
-    else:
-        logger.info('All dependencies satisfied!')
+    def check_or_fail(self):
+        missing = self.handle()
+        if missing:
+            self.logger.error('Install dependencies to continue; exiting')
+            sys.exit(1)
 
-    return missing
+    def check_system_path(self):
+        
+        deps = {}
+
+        hmmscan = which('hmmscan')
+        hmmpress = which('hmmpress')
+        if hmmscan is None or hmmpress is None:
+            deps['HMMER'] = False
+        else:
+            deps['HMMER'] = True
+            self.logger.debug('hmmscan:' + hmmscan)
+            self.logger.debug('hmmpress:' + hmmpress)
+
+        cmscan = which('cmscan')
+        cmpress = which('cmpress')
+        if cmscan is None or cmpress is None:
+            deps['Infernal'] = False
+        else:
+            deps['Infernal'] = True
+            self.logger.debug('cmscan:' + cmscan)
+            self.logger.debug('cmpress:' + cmpress)
+
+        blastp = which('blastp')
+        blastx = which('blastx')
+        tblastn = which('tblastn')
+        makeblastdb = which('makeblastdb')
+        if (blastp is None) or (blastx is None) \
+            or (tblastn is None) or (makeblastdb is None):
+            
+            deps['BLAST+'] = False
+        else:
+            deps['BLAST+'] = True
+            self.logger.debug('blastp:' + blastp)
+            self.logger.debug('blastx:' + blastx)
+            self.logger.debug('tblastn:' + tblastn)
+            self.logger.debug('makeblastdb:' + makeblastdb)
+
+        busco = which('BUSCO_v1.1b1.py')
+        if busco is None:
+            deps['BUSCO'] = False
+        else:
+            deps['BUSCO'] = True
+            self.logger.debug('BUSCO:' + busco)
+
+
+        longorfs = which('TransDecoder.LongOrfs')
+        predict = which('TransDecoder.Predict')
+        if longorfs is None or predict is None:
+            deps['TransDecoder'] = False
+        else:
+            deps['TransDecoder'] = True
+            self.logger.debug('TransDecoder.LongOrfs:' + longorfs)
+            self.logger.debug('TransDecoder.Predict:' + predict)
+
+        lastdb = which('lastdb')
+        lastal = which('lastal')
+        if lastdb is None or lastal is None:
+            deps['LAST'] = False
+        else:
+            deps['LAST'] = True
+            self.logger.debug('lastal:' + lastal)
+            self.logger.debug('latsdb:' + lastdb)
+
+        crb_blast = which('crb-blast')
+        if crb_blast is None:
+            deps['crb-blast'] = False
+        else:
+            deps['crb-blast'] = True
+            self.logger.debug('crb-blast:' + crb_blast)
+
+        return deps
+
 
