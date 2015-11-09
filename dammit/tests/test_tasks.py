83373d56f7ed4f458716e4750db46bca1d51229c
Camille Scott <camille.scott.w@gmail.com>
2015-11-09 01:21:07 -0800

    Add basic HMMER tests and test data

--- dammit/tests/test_tasks.py
+++ dammit/tests/test_tasks.py
@@ -16,7 +16,7 @@ from dammit.common import run_tasks
 BATCH EFFECTS -- The Notorious A.T.G.
 -------------------------------------
 
-Shit your 'scripts ain't differential --
+Shit your 'scripts ain't differential -
 they're preferential
 (-ly selected!)
 from you read seq
@@ -72,8 +72,6 @@ class TestLASTTasks(TestCase):
                     run_tasks([task], ['run'])
                     status = check_status(task)
                     print(os.listdir(td), file=sys.stderr)
-                    print('PATH:', os.environ['PATH'], file=sys.stderr)
-
                     
                     for ext in self.extensions:
                         self.assertTrue(os.path.isfile(tf + ext))
@@ -160,3 +158,59 @@ class TestLASTTasks(TestCase):
                     run_tasks([aln_task], ['run'])
                     status = check_status(aln_task)
                     self.assertEquals(status.status, 'up-to-date')
+
+
+class TestHMMERTasks(TestCase):
+
+    @classmethod
+    def setup_class(cls):
+        cls.hmmscan_cfg = common.CONFIG['settings']['hmmer']['hmmscan']
+        cls.hmmpress_cfg = common.CONFIG['settings']['hmmer']['hmmscan']
+        cls.extensions = ['.h3f', '.h3i', '.h3m', '.h3p']
+
+    def test_hmmpress_task(self):
+        with TemporaryDirectory() as td:
+            with Move(td):
+                with TestData('test-profile.hmm', td) as tf:
+                    task = tasks.get_hmmpress_task(tf, self.hmmpress_cfg)
+                    run_tasks([task], ['run'])
+                    status = check_status(task)
+                    print(os.listdir(td), file=sys.stderr)
+                    
+                    for ext in self.extensions:
+                        self.assertTrue(os.path.isfile(tf + ext))
+
+                    self.assertEquals(status.status, 'up-to-date')
+    
+    def test_hmmpress_task_existing(self):
+        with TemporaryDirectory() as td:
+            with Move(td):
+                with TestData('test-profile.hmm', td) as tf:
+                    for ext in self.extensions:
+                        touch(tf + ext)
+
+                    task = tasks.get_hmmpress_task(tf, self.hmmpress_cfg)
+                    run_tasks([task], ['run'])
+                    print(os.listdir(td), file=sys.stderr)
+                    print(task, file=sys.stderr)
+                    status = check_status(task)
+                    
+                    self.assertEquals(status.status, 'up-to-date')
+
+    def test_hmmscan_task(self):
+        with TemporaryDirectory() as td:
+            with Move(td):
+                with TestData('test-protein.fa', td) as prot, \
+                     TestData('test-profile.hmm', td) as hmm, \
+                     TemporaryFile(td) as out:
+                        
+                    db_task = tasks.get_hmmpress_task(hmm, self.hmmpress_cfg)
+                    aln_task = tasks.get_hmmscan_task(prot, out, hmm, 1,
+                                                      self.hmmscan_cfg)
+                    run_tasks([db_task, aln_task], ['run'])
+                    print(os.listdir(td), file=sys.stderr)
+                    aln = ''.join(open(out).readlines())
+                    print(aln, file=sys.stderr)
+
+                    self.assertEquals(aln.count('accession'), 2)
+                    self.assertIn('i-Evalue', aln)
