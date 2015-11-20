3cd9f2d9c2233f12bd3e49e54106410174955a2b
Camille Scott <camille.scott.w@gmail.com>
2015-11-19 16:56:56 -0800

    Change name of model.py to crbl.py; add BestHits class for generalized best hits stuff

--- dammit/crbl.py
+++ dammit/crbl.py
@@ -19,6 +19,83 @@ from . import common
 from . import parsers
 from . import tasks
 
+
+class BestHits(object):
+
+    def __init__(self, comparison_col='E', query_name_col='q_name', 
+                 subject_name_col='s_name', query_length_col='q_len',
+                 subject_length_col='q_len'):
+        self.comparison_col = comparison_col
+        self.query_name_col = query_name_col
+        self.subject_name_col = subject_name_col
+        self.query_length_col = query_length_col
+        self.subject_length_col = subject_length_col
+
+    def best_hits(aln_df):
+        '''Get the best hit for each query in the alignment DataFrame.
+
+        Operates in-place. Sorts the hits by query name and then e-value,
+        and then uses the drop_duplicates() function to remove all but the
+        first (lowest e-value) hit for each query.
+
+        Args:
+            aln_df (DataFrame): The MAF alignment DataFrame.
+            comp_col (str): The column name to use for sorting hits.
+        '''
+
+        return aln_df.sort_values(
+                   by=[self.query_name_col,self.comparison_col]
+                ).drop_duplicates(subset=self.query_name_col)
+
+    def reciprocal_best_hits(aln_df_A, aln_df_B):
+        '''Given to DataFrames with reciprocal MAF alignments, get the
+        reciprocal best hits.
+
+        Uses the CRBL.best_hits function to get best hits for each DataFrame,
+        and then does an inner join to find the reciprocals. The DataFrames
+        *will* be modified in-place by the best_hits function!
+
+        Args:
+            aln_df_A (DataFrame): The query hits.
+            aln_df_B (DataFrame): The subject hits.
+            comp_col (str): The comparison column to use for best hits.
+        Returns:
+            DataFrame with the reciprocal best hits.
+        '''
+
+        aln_df_A = CRBL.best_hits(aln_df_A)
+        aln_df_B = CRBL.best_hits(aln_df_B)
+
+        # Join between subject A and query B
+        rbh_df = pd.merge(aln_df_A, aln_df_B, how='inner', 
+                          left_on=self.subject_name_col, 
+                          right_on=self.query_name_col)
+
+        # Renamed columns after join
+        left_q_col = self.query_name_col + '_x'
+        left_s_col = self.subject_name_col + '_x'
+        left_len_col = self.query_length_col + '_x'
+        left_comp_col = self.comparison_col + '_x'
+        right_q_col = self.query_name_col + '_y'
+        right_s_col = self.subject_name_col + '_y'
+        right_len_col = self.query_len_col + '_y'
+
+        # Select those where query A is the same as subject B
+        rbh_df = rbh_df[(rbh_df[left_q_col] == rbh_df[right_s_col])]
+
+        # Drop extra columns and rename for clarity
+        del rbh_df[left_s_col]
+        del rbh_df[right_s_col]
+        rbh_df.rename(columns={left_q_col: self.query_name_col, 
+                               right_q_col: self.subject_name_col,
+                               left_comp_col: self.comparison_col,
+                               left_len_col: self.query_length_col,
+                               right_len_col: self.subject_length_col},
+                      inplace=True)
+        
+        return rbh_df
+
+
 class CRBL(object):
     '''
     TODO:
@@ -62,62 +139,9 @@ class CRBL(object):
         self.lastal_cfg = lastal_cfg
         self.lastdb_cfg = lastdb_cfg
 
-    @staticmethod
-    def best_hits(aln_df, comp_col='E'):
-        '''Get the best hit for each query in the alignment DataFrame.
+        self.best_hits = BestHits()
 
-        Operates in-place. Sorts the hits by query name and then e-value,
-        and then uses the drop_duplicates() function to remove all but the
-        first (lowest e-value) hit for each query.
-
-        Args:
-            aln_df (DataFrame): The MAF alignment DataFrame.
-            comp_col (str): The column name to use for sorting hits.
-        '''
-
-        aln_df.sort(columns=['q_name', comp_col], inplace=True)
-        aln_df.drop_duplicates(subset='q_name', inplace=True)
-
-    @staticmethod
-    def reciprocal_best_hits(aln_df_A, aln_df_B, comp_col='E'):
-        '''Given to DataFrames with reciprocal MAF alignments, get the
-        reciprocal best hits.
-
-        Uses the CRBL.best_hits function to get best hits for each DataFrame,
-        and then does an inner join to find the reciprocals. The DataFrames
-        *will* be modified in-place by the best_hits function!
-
-        Args:
-            aln_df_A (DataFrame): The query hits.
-            aln_df_B (DataFrame): The subject hits.
-            comp_col (str): The comparison column to use for best hits.
-        Returns:
-            DataFrame with the reciprocal best hits.
-        '''
-
-        CRBL.best_hits(aln_df_A)
-        CRBL.best_hits(aln_df_B)
-
-        # Join between subject A and query B
-        rbh_df = pd.merge(aln_df_A, aln_df_B, how='inner', 
-                          left_on='s_name', right_on='q_name')
-        # Select those where query A is the same as subject B
-        rbh_df = rbh_df[(rbh_df['q_name_x'] == rbh_df['s_name_y'])]
-
-        # Drop extra columns and rename for clarity
-        del rbh_df['s_name_x']
-        del rbh_df['s_name_y']
-        rbh_df.rename(columns={'q_name_x': 'q_name', 
-                               'q_name_y': 's_name',
-                               'q_aln_len_x': 'q_aln_len',
-                               'E_x': 'E',
-                               'q_len_x': 'q_len',
-                               'q_len_y': 's_len'},
-                      inplace=True)
-        
-        return rbh_df
-
-    def preprocess(self, alignments):
+     def preprocess(self, alignments):
 
         # Subset out the features
         data = alignments.dropna()[self.features].astype(float)
