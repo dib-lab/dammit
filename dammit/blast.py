1699775ae5676a38b0886cead1cba45c72b1ba21
Camille Scott <camille.scott.w@gmail.com>
2016-01-05 16:39:45 -0800

    Remove cython blast coord converters

--- dammit/blast.py
+++ dammit/blast.py
@@ -1,41 +1,69 @@
 #!/usr/bin/env python
-#import pyximport
-import numpy
-#pyximport.install(setup_args={"include_dirs":numpy.get_include()}, reload_support=True)
-from . import cblast
+import numpy as np
 import csv
 import sys
 import pandas as pd
 
-def best_hits(df, comp_col='evalue'):
-    assert type(df) is pd.DataFrame
-    df['ind'] = df.index
-    df.sort(columns=['ind', comp_col], inplace=True)
-    df.drop_duplicates(subset='ind', inplace=True)
-    del df['ind']
 
-def get_orthologies(A, B, idx, comp_col='evalue'):
+def fix_coords_single(sstart, send, qstart, qend):
+    '''Fix BLAST coordinates for the parameters from a singe hit.
 
-    assert type(A) is pd.DataFrame
-    assert type(B) is pd.DataFrame
-    assert type(idx) is pd.Index
+    Args:
+        sstart (long): Subject start.
+        send (long): Subject end.
+        qstart (long): Query start.
+        qend (long): Query end.
+    Returns:
+        ndarray: Numpy array with values [sstart, send, qstart, qend, sstrand, qstrand],
+        where sstrand and qstrand are the subject and query strands as a 1 or -1.
+    '''
+    res = np.empty(6, dtype=long)
+    
+    if sstart < send:
+        res[0] = sstart - 1
+        res[1] = send
+        res[4] = 1
+    else:
+        res[0] = send
+        res[1] = sstart + 1
+        res[4] = -1
+    
+    if qstart < qend:
+        res[2] = qstart - 1
+        res[3] = qend
+        res[5] = 1
+    else:
+        res[2] = qend
+        res[3] = qstart + 1
+        res[5] = -1
+    
+    return res
 
-    best_hits(A)
-    best_hits(B)
 
-    X = pd.merge(A, B, how='inner', left_on='sseqid', right_index=True)
-    X = pd.merge(pd.DataFrame(index=idx),
-                      X[(X.index == X.sseqid_y)], left_index=True, right_index=True, how='left')
-    del X['sseqid_x']
-    del X['sseqid_y']
+def fix_blast_coords(sstart, send, qstart, qend):
+    '''Fix BLAST coordinates of many hits.
 
-    return X
+    Expects each column of attributes separately as numpy arrays.
+
+    Args:
+        sstart (ndarray): The subject start coordinates.
+        send (ndarray): The subject end coordinates.
+        qstart (ndarray): The query start coordinates.
+        qend (ndarray): The query end coordinates.
+    '''
+    n = len(sstart)
+    i = 0
+    res = np.empty((n,6), dtype=long)
+    for i in range(n):
+        res[i,:] = fix_coords_single(sstart[i], send[i], qstart[i], qend[i])
+    return res
+
 
 def remap_blast_coords_df(df):
-    coords = cblast.fix_blast_coords(df.sstart.values, df.send.values, df.qstart.values, df.qend.values)
+    coords = fix_blast_coords(df.sstart.values, df.send.values, df.qstart.values, df.qend.values)
     df['sstart'] = coords[:,0]
     df['send'] = coords[:,1]
     df['qstart'] = coords[:,2]
     df['qend'] = coords[:,3]
-    df['sstrand'] = coords[:,4]
+    df['sstrand'] = coords[coords:,4]
     df['qstrand'] = coords[:,5]
--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@
 
 import sys, platform
 
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
-
 # Automatically download setuptools if not available
 try:
     from setuptools import *
@@ -42,12 +35,10 @@ def main():
             tests_require = ['nose'],
             packages = ['dammit'],
             scripts = glob('bin/*'),
-            ext_modules = get_extension_modules(),
             install_requires = ['setuptools>=0.6.35',
                                 'pandas>=0.17',
                                 'khmer>=2.0',
                                 'doit>=0.29.0',
-                                #'sphinxcontrib-napoleon>=0.3.11',
                                 'Sphinx>1.3.1',
                                 'sphinx-rtd-theme>=0.1.9',
                                 'nose==1.3.4',
@@ -57,29 +48,7 @@ def main():
                                 'scikit-learn>=0.16',
                                 'numexpr>=2.3.1'],
             include_package_data = True,
-            zip_safe = False,
-            cmdclass = cmdclass  )
-
-def get_extension_modules():
-    extensions = []
-    
-    if use_cython:
-        ext = '.pyx'
-        cmdclass.update({ 'build_ext': build_ext })
-    else:
-        ext = '.c'
-
-    extensions.append( Extension( "dammit.cblast", [ "dammit/cblast" + ext],
-                                  include_dirs=['.'] + [numpy.get_include()] ) )
-    return extensions     
-
-def monkey_patch_numpy():
-    try:
-        import numpy
-        numpy.test = None
-    except:
-        pass
+            zip_safe = False,  )
         
 if __name__ == "__main__":
-    monkey_patch_numpy()
     main()
