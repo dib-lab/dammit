9d2e6bd2df8ea974737d5f2884adf4cb2718388a
Camille Scott <camille.scott.w@gmail.com>
2016-04-27 18:24:01 -0700

    Add ui module and tests

---
+++ dammit/tests/test_ui.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import json
+import os
+import sys
+
+from unittest import TestCase
+
+from dammit.ui import header, checkbox, paragraph, listing
+
+class TestUIFunctions(TestCase):
+
+
+    def test_header(self):
+        self.assertEquals(header('test'), '# test')
+        self.assertEquals(header('test', level=0), '# test')
+        self.assertEquals(header('test', level=2), '## test')
+
+    def test_paragraph(self):
+        long_text = 'damn' * 160
+        self.assertEquals(paragraph(long_test), 'damn' * 80 + 'n' + 'damn' * 80)
+
+    def test_listing(self):
+        d = {'a': 2, 'b': 5, 'c': 10}
+        exp_d = '* a: 2\n* b: 5\n* c: 10'
+        exp_l = '* a\n* b\n* c'
+
+        self.assertEquals(listing(d), exp_d)
+        self.assertEquals(listing(d.keys()), exp_l)
+        with self.assertRaises(TypeError):
+            listing('test')
+
+    def test_checkbox(self):
+        self.assertEquals(checkbox('test'), '[ ] test')
+        self.assertEquals(checkbox('test', checked=True), '[x] test')
--- dammit/ui.py
+++ dammit/ui.py
@@ -1,27 +1,34 @@
-def add_header(msg, level):
-    s = ''
-    if level == 0:
-        symbol = '='
-        return '{0}\n{1}\n{2}\n'.format(symbol * 40,
-                                      msg,
-                                      symbol * 40)
-    elif level == 1:
-        symbol = '~'
-        return '{0}\n{1}\n{2}'.format(symbol * 30,
-                                      msg,
-                                      symbol * 30)
-    else:
-        symbol = '---'
-        return '\n{0} {1}\n'.format(symbol,
-                                    msg)
+from __future__ import print_function
 
+import sys
+import textwrap
 
-def print_header(msg, level):
+def header(msg, level=1):
     '''Standardize output headers for submodules.
 
     This doesn't need to be logged, but it's nice for
     the user.
     '''
-    print(add_header(msg, level), file=sys.stderr)
+    if level < 1:
+        level = 1
+    return ' '.join([level * '#', msg])
 
 
+def checkbox(msg, checked=False):
+    if checked:
+        return '[x] ' + msg
+    else:
+        return '[ ] ' + msg
+
+
+def paragraph(msg, wrap=80):
+    return '\n'.join(textwrap.wrap(msg, wrap))
+
+ 
+def listing(d):
+    if type(d) is dict:
+        return '\n'.join(['* {k}: {v}'.format(k=key,v=val) for key, val in d.items()])
+    elif type(d) is list:
+        return '\n'.join(['* {0}'.format(e) for e in d])
+    else:
+        raise TypeError('Cannot make {0} into listing'.format(d))
