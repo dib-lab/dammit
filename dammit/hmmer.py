187e48cfbbfed13e8ed226331de1e391e372e5e5
Camille Scott <camille.scott.w@gmail.com>
2016-05-30 11:38:39 -0700

    remove pathlib from hmmer

--- dammit/hmmer.py
+++ dammit/hmmer.py
@@ -7,52 +7,38 @@ from doit.task import clean_targets
 import os
 import pandas as pd
 
-from .parallel import parallel_fasta, multinode_parallel_fasta
-from .parallel import get_filesize_task
+from .parallel import parallel_fasta
 from . import parsers
 from .utils import doit_task, which, convert_pathlib
 
 
 @doit_task
-@convert_pathlib
 def get_hmmscan_task(input_filename, output_filename, db_filename,
-                     cutoff=0.00001, n_threads=1, n_nodes=None, 
+                     cutoff=0.00001, n_threads=1, pbs=False, 
                      params=None):
 
     name = 'hmmscan:' + os.path.basename(input_filename) + '.x.' + \
                     os.path.basename(db_filename)
     stat = output_filename + '.hmmscan.out'
-    def hmmscan_cmd(file_size):
-
-        hmmscan_exc = which('hmmscan')
-        
-        if n_nodes is None:
-            parallel_cmd = parallel_fasta(input_filename, n_threads, file_size)
-        else:
-            parallel_cmd = multinode_parallel_fasta(input_filename, n_threads,
-                                                    n_nodes, file_size)
-
-        cmd = [parallel_cmd, hmmscan_exc]
-        if params is not None:
-            cmd.extend([str(p) for p in params])
-        cmd.extend(['--cpu', '1', '--domtblout', '/dev/stdout', 
-                    '-E', str(cutoff), '-o', stat, db_filename, '/dev/stdin',
-                    '>', output_filename])
-
-        return ' '.join(cmd)
+    
+    hmmscan_exc = which('hmmscan')
+    cmd = [hmmscan_exc]
+    if params is not None:
+        cmd.extend([str(p) for p in params])
+    cmd.extend(['--cpu', '1', '--domtblout', '/dev/stdout', 
+                '-E', str(cutoff), '-o', stat, db_filename, '/dev/stdin'])
+    
+    cmd = parallel_fasta(input_filename, output_filename, cmd, n_threads, pbs=pbs)
         
     return {'name': name,
             'title': title_with_actions,
-            'actions': [LongRunning(hmmscan_cmd)],
+            'actions': [LongRunning(cmd)],
             'file_dep': [input_filename, db_filename, db_filename+'.h3p'],
             'targets': [output_filename, stat],
-            'getargs': {'file_size': ('get_filesize:{0}'.format(input_filename),
-                                      'size')},
             'clean': [clean_targets]}
 
 
 @doit_task
-@convert_pathlib
 def get_hmmpress_task(db_filename, params=None, task_dep=None):
 
     name = 'hmmpress:' + os.path.basename(db_filename)
@@ -106,13 +92,3 @@ def get_remap_hmmer_task(hmmer_filename, remap_gff_filename, output_filename):
             'targets': [output_filename],
             'clean': [clean_targets]}
 
-
-def hmmscan(input_filename, output_filename, db_filename, **hmmscan_kwds):
-    '''Wrapper for `get_hmmscan_task`. Yields a filesize_task
-    and an hmmscan_task.'''
-
-
-
-    yield get_filesize_task(input_filename)
-    yield get_hmmscan_task(input_filename, output_filename, db_filename,
-                           **hmmscan_kwds)
