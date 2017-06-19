36537270f00b9abf14437d54acbd117ec3d61a0c
Camille Scott <camille.scott.w@gmail.com>
2017-06-19 15:38:32 -0700

    convert transdecoder to pytest

--- dammit/tests/test_transdecoder.py
+++ dammit/tests/test_transdecoder.py
@@ -3,68 +3,69 @@ from __future__ import print_function
 
 import json
 import os
+from pprint import pprint
 import sys
 
-from unittest import TestCase
 from doit.dependency import Dependency, DbmDB
 
-from utils import TemporaryDirectory, Move, TestData, touch, TemporaryFile
+from utils import touch, datadir
 from utils import run_task, run_tasks, check_status
-from dammit.tasks.transdecoder import (get_transdecoder_orf_task,
-                                       get_transdecoder_predict_task)
+from dammit.tasks.transdecoder import (TransDecoderLongOrfsTask,
+                                       TransDecoderPredictTask)
 from dammit.meta import get_config
 
+import pytest
 
-class TestTransDecoderTasks(TestCase):
+
+class TestTransDecoderTasks():
 
     @classmethod
     def setup_class(cls):
         cfg, _ = get_config()
         cls.longorfs_cfg = cfg['transdecoder']['longorfs']
         cls.predict_cfg = cfg['transdecoder']['predict']
-        cls.extensions = ['.bed', '.pep', '.gff3', '.mRNA', '.cds']
+        cls.extensions = ['.bed', '.pep', '.gff3', '.cds']
 
-    def test_longorfs_task(self):
-        with TemporaryDirectory() as td:
-            with Move(td):
-                with TestData('test-transcript.fa', td) as transcript, \
-                     TestData('test-transcript-orf.pep', td) as exp_orf:
+    def test_longorfs_task(self, tmpdir, datadir):
+        with tmpdir.as_cwd():
+            transcript = datadir('test-transcript.fa')
+            exp_orf = datadir('test-transcript-orf.pep')
 
-                    task = get_transdecoder_orf_task(transcript,
-                                                           self.longorfs_cfg)
-                    run_tasks([task], ['run'])
-                    output_dir = transcript + '.transdecoder_dir'
+            task = TransDecoderLongOrfsTask().task(transcript,
+                                                   params=self.longorfs_cfg)
+            run_tasks([task], ['run'])
+            output_dir = transcript + '.transdecoder_dir'
 
-                    exp_pep = open(exp_orf).read()
+            exp_pep = open(exp_orf).read()
 
-                    pep_fn = os.path.join(output_dir, 'longest_orfs.pep')
-                    self.assertTrue(os.path.isfile(pep_fn))
-                    pep = open(pep_fn).read()
+            pep_fn = os.path.join(output_dir, 'longest_orfs.pep')
+            assert os.path.isfile(pep_fn)
+            pep = open(pep_fn).read()
 
-                    self.assertIn(exp_pep, pep)
+            assert exp_pep in pep
 
-    def test_predict_task(self):
-        with TemporaryDirectory() as td:
-            with Move(td):
-                with TestData('test-transcript.fa', td) as transcript, \
-                     TestData('test-protein-x-pfam-a.tbl', td) as pfam:
+    def test_predict_task(self, tmpdir, datadir):
+        with tmpdir.as_cwd():
+            transcript = datadir('test-transcript.fa')
+            pfam = datadir('test-protein-x-pfam-a.tbl')
 
-                    orf_task = get_transdecoder_orf_task(transcript,
-                                                               self.longorfs_cfg)
-                    pred_task = get_transdecoder_predict_task(transcript,
-                                                                    pfam,
-                                                                    self.predict_cfg)
-                    run_tasks([orf_task, pred_task], ['run'])
-                    
-                    for ext in self.extensions:
-                        fn = os.path.join(td, transcript+'.transdecoder'+ext)
-                        self.assertTrue(os.path.isfile(fn))
-                        contents = open(fn).read()
-                        if ext == '.gff3':
-                            self.assertIn('mRNA', contents)
-                            self.assertIn('gene', contents)
-                            self.assertIn('CDS', contents)
-                            self.assertIn('three_prime_UTR', contents)
-                            self.assertIn('exon', contents)
+            orf_task = TransDecoderLongOrfsTask().task(transcript,
+                                                       params=self.longorfs_cfg)
+            pred_task = TransDecoderPredictTask().task(transcript,
+                                                       pfam,
+                                                       params=self.predict_cfg)
+            run_tasks([orf_task, pred_task], ['run'])
+            
+            pprint(tmpdir.listdir())
+            for ext in self.extensions:
+                fn = transcript+'.transdecoder'+ext
+                assert os.path.isfile(fn)
+                contents = open(fn).read()
+                if ext == '.gff3':
+                    assert 'mRNA' in contents
+                    assert 'gene' in contents
+                    assert 'CDS' in contents
+                    assert 'three_prime_UTR' in contents
+                    assert 'exon' in contents
 
 
