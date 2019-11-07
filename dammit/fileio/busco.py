f28101e99d61dca1779a9a403a5d704421b9e3bb
Camille Scott <camille.scott.w@gmail.com>
2019-11-06 20:03:52 -0800

    move busco parsers to fileio submodule

---
+++ dammit/fileio/busco.py
@@ -0,0 +1,102 @@
+# Copyright (C) 2015-2018 Camille Scott
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
+
+
+
+def parse_busco_full(fn):
+    '''Parses a BUSCO full result table into a Pandas DataFrame.
+
+    Args:
+        fn (str): The results file.
+    Returns:
+        DataFrame: The results DataFrame.
+    '''
+
+    df = pd.read_table(fn)
+    return df.rename(columns={'#BUSCO_group': 'BUSCO_group'})
+
+
+def parse_busco_summary(fn):
+    '''Parses a BUSCO summary file into a JSON compatible
+    dictionary.
+
+    Args:
+        fn (str): The summary results file.
+    Returns:
+        dict: The BUSCO results.
+    '''
+
+    res = {}
+    with open(fn) as fp:
+        for ln in fp:
+            if ln.strip().startswith('C:'):
+                tokens = ln.split(',')
+                for token in tokens:
+                    key, _, val = token.partition(':')
+                    key = key.strip()
+                    val = val.strip().strip('%')
+                    if key == 'C':
+                        valc, _, vald = val.partition('%')
+                        valc = valc.strip()
+                        vald = vald.strip('D:][%')
+                        res['C(%)'] = valc
+                        res['D(%)'] = vald
+                    else:
+                        if key != 'n':
+                           key += '(%)'
+                        res[key] = val.strip().strip('%')
+    return res
+
+
+def parse_busco_multiple(fn_list, dbs=['metazoa', 'vertebrata']):
+    '''Parses multiple BUSCO results summaries into an appropriately
+    index DataFrame.
+
+    Args:
+        fn_list (list): List of paths to results files.
+        dbs (list): List of BUSCO database names.
+    Returns:
+        DataFrame: The formated DataFrame.
+    '''
+
+    data = []
+    for fn in fn_list:
+        data.append(parse_busco_summary(fn))
+
+    df = pd.DataFrame(data)
+    df['fn'] = [os.path.basename(fn)[14:-14].strip('.') for fn in fn_list]
+    df['db'] = None
+    for db in dbs:
+        idx = df.fn.str.contains(db)
+        df.loc[idx,'db'] = db
+        df.loc[idx,'fn'] = df.loc[idx, 'fn'].apply(lambda fn: fn[:fn.find(db)].strip('. '))
+
+    return df
+
+
+def busco_to_df(fn_list, dbs=['metazoa', 'vertebrata']):
+    ''' Given a list of BUSCO results from different databases, produce
+    an appropriately multi-indexed DataFrame of the results.
+
+    Args:
+        fn_list (list): The BUSCO summary files.
+        dbs (list): The BUSCO databases used for these runs.
+    Returns:
+        DataFrame: The BUSCO results.
+    '''
+
+    data = []
+    for fn in fn_list:
+        data.append(parse_busco(fn))
+
+    df = pd.DataFrame(data)
+    df['fn'] = [os.path.basename(fn)[14:-14].strip('.') for fn in fn_list]
+    df['db'] = None
+    for db in dbs:
+        idx = df.fn.str.contains(db)
+        df.loc[idx,'db'] = db
+        df.loc[idx,'fn'] = df.loc[idx, 'fn'].apply(lambda fn: fn[:fn.find(db)].strip('. '))
+    return df
