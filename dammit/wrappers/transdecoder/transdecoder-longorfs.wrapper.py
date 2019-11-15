9f719ebfa9bd838469feab7c4fcfa4c9085d7b6d
Tessa Pierce <ntpierce@gmail.com>
2019-11-15 11:06:26 -0800

    add transdecoder, last, infernal, hmmer wrappers

--- dammit/wrappers/transdecoder/transdecoder-longorfs.wrapper.py
+++ dammit/wrappers/transdecoder/transdecoder-longorfs.wrapper.py
@@ -1,7 +1,7 @@
-"""Snakemake wrapper for Transdecoder"""
+"""Snakemake wrapper for Transdecoder LongOrfs"""
 
-__author__ = "N Tessa Pierce"
-__copyright__ = "Copyright 2019, Tessa Pierce"
+__author__ = "N. Tessa Pierce"
+__copyright__ = "Copyright 2019, N. Tessa Pierce"
 __email__ = "ntpierce@gmail.com"
 __license__ = "MIT"
 
@@ -19,13 +19,12 @@ if gtm:
 
 output_dir = path.dirname(str(snakemake.output))
 
-
 # transdecoder fails if output already exists. No force option available
 shell("rm -rf {output_dir}")
 
 input_fasta = str(snakemake.input.fasta)
-if input_fasta.endswith('gz'):
-    input_fa = input_fasta.rsplit('.gz')[0]
+if input_fasta.endswith("gz"):
+    input_fa = input_fasta.rsplit(".gz")[0]
     shell("gunzip -c {input_fasta} > {input_fa}")
 else:
     input_fa = input_fasta
