bc829405c78753169dc73839416caf923ebfda80
Camille Scott <camille.scott.w@gmail.com>
2021-02-11 19:43:35 -0800

    Update generate-test-data.sh with --regex-rename

--- generate-test-data.sh
+++ generate-test-data.sh
@@ -5,14 +5,34 @@ TEST_FILE=pom.20.fa
 TEST_NAME=pom.20
 TEST_PEP=pep.fa
 
+#
+# Execute
+#
 
+# Default pipeline and params
 dammit run  --busco-group saccharomycetes_odb10  --n-threads 4 annotate $DATA_DIR/$TEST_FILE 
-#dammit run --pipeline full annotate $DATA_DIR/$TEST_FILE -o $TEST_NAME.dammit.full 
-#dammit annotate $DATA_DIR/$TEST_FILE --nr -o $TEST_FILE.dammit.nr 
-dammit run  --busco-group saccharomycetes_odb10  --n-threads 4 annotate --global-evalue 10.0 $DATA_DIR/$TEST_FILE -o $TEST_NAME.dammit.evalue10 
+
+# Full pipeline, -o
+#dammit run --pipeline full annotate $DATA_DIR/$TEST_FILE -o $TEST_NAME.dammit.full
+
+# Global evalue cutoff
+dammit run  --busco-group saccharomycetes_odb10  --n-threads 4 annotate --global-evalue 10.0 $DATA_DIR/$TEST_FILE -o $TEST_NAME.dammit.evalue10
+
+# Passing a user database
 dammit run  --busco-group saccharomycetes_odb10  --n-threads 4 --pipeline quick annotate $DATA_DIR/$TEST_FILE --user-database $DATA_DIR/$TEST_PEP -o $TEST_NAME.dammit.udb 
+
+# Passing multiple musco groups
 dammit run --n-threads 4 --pipeline quick --busco-group bacteria_odb10 --busco-group saccharomycetes_odb10 annotate -o pom.256.dammit.busco-multi $DATA_DIR/pom.256.fa 
+
+# Using --regex-rename
+dammit run --busco-group saccharomycetes_odb10 --pipeline quick annotate --regex-rename "(?P<name>^[a-zA-Z0-9\.]+)" -o pom.20.dammit.regex dammit/tests/test-data/pom.20.fa
+
+# Rename with backmapping
 #dammit run annotate $DATA_DIR/$TEST_FILE --no-rename -o $TEST_FILE.dammit.norename
+
+#
+# Copy the data
+#
 
 cp $TEST_NAME.dammit/$TEST_NAME.dammit.fasta $DATA_DIR/ 
 cp $TEST_NAME.dammit/$TEST_NAME.dammit.gff3 $DATA_DIR/
